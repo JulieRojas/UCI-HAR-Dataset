74ff0f9220e9f1e0ee47a14b72e08fd901c4f761
Julie Rojas <julie.cm.rojas@gmail.com>
2020-06-01 02:05:11 +0200

    Almost there, Question 5 missing

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,53 @@
-# Loading features names 
-feature_names <- read.table('features.txt')
-# Loading all train data with column names
-train_X <- read.table('train/X_train.txt', col.names = feature_names[,2])
-#train_y <- read.table('train/y_train.txt', col.names = y_label)
-#train_subject <- read.table('train/subject_train.txt', col.names = subject)
+library(dplyr)  
+  # Loading features names 
+  feature_names <- read.table('features.txt')
+  # Loading test and train X with column names from feature names
+  train_X <- read.table('train/X_train.txt', col.names = feature_names[,2])
+  test_X <- read.table('test/X_test.txt', col.names = feature_names[,2])
+  # Combine test set and train set
+  X <- rbind(train_X, test_X)
+  # Get all the column names to lower case.
+  colnames(X)<- tolower(colnames(X))
+  # Finding the column for mean and standard deviation
+  m <- grep("mean", colnames(X))
+  s <- grep("std", colnames(X))
+  col <- sort(append(m, s))
+  # Select columns for mean ans standard deviation 
+  Xmean_std <- X[, col]
+  # removing meanfreq because it's different from mean
+  f <- grep("meanfreq", colnames(Xmean_std))
+  Xmean_std <- Xmean_std[,-f]
+  
+  # 4) Formatting column names.
+  colnames(Xmean_std)<- gsub("\\.", "", colnames(Xmean_std))
+  colnames(Xmean_std)<- gsub("bodybody", "body", colnames(Xmean_std))
+  print(colnames(Xmean_std))
+  
+  # 3) labels data formatting
+  labels_train <- read.table('train/y_train.txt', col.names = c("activity"))
+  labels_test <- read.table('test/y_test.txt', col.names = c("activity"))
+  # merge
+  labels <- rbind(labels_train, labels_test)
+  # getting the labels info
+  labels_info <- read.table('activity_labels.txt', col.names = c("number", "activity"))
+  labels$activity <- as.character(labels$activity)
+  for(num in labels_info$number){
+    labels[labels == as.character(num)] <- tolower(as.character(labels_info[num, 2]))
+  }
+  
+  # Join activity labels to X dataset 
+  # (it was not asked, but I think it makes sense)
+  Dataset <- cbind(Xmean_std, labels)
+  # Save that dataset
+  write.table(Dataset, "Dataset_formatted.txt")
+  
+  # 5) From the data set in step 4, creates a second, independent tidy data set
+  #with the average of each variable for each activity and each subject.
+  train_subject <- read.table('train/subject_train.txt', col.names = c("subject"))
+  test_subject <- read.table('test/subject_test.txt', col.names = c("subject"))
+  # Combine subjects test set and train set
+  subject <- rbind(train_subject, test_subject)
+  # Combine subject to Dataset
+  Dataset <- cbind(Dataset, subject)
+  
 
-# Loading all test data with column names
-test_X <- read.table('test/X_test.txt', col.names = feature_names[,2])
-#test_y <- read.table('test/y_test.txt', col.names = y_label)
-#test_subject <- read.table('test/subject_test.txt', col.names = subject) 
-
-# Combine test set and train set
-X <- rbind(train_X, test_X)
